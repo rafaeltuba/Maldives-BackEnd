fb427e0fdfde1117ea6ac7cbb513deb314e48664
rafaeltuba <rafael.tuba@gmail.com>
2018-07-18 20:04:05 +0200

    junit

--- src/com/maldives/model/Empresa.java
+++ src/com/maldives/model/Empresa.java
@@ -1,5 +1,87 @@
 package com.maldives.model;
 
+import java.util.ArrayList;
+
 public class Empresa {
+	
+	private String nmEmpresa;
+	private String cdRamoAtividade;
+	private String deEmail;
+	private String deSenha;
+
+	public void setNmEmpresa(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCdRamoAtividade(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCdMunicipio(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAboutUs(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEspecialidades(ArrayList arrayList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDeSite(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCdSede(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCdTipo(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setNuAnoFundacao(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDeEmail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDeSenha(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getCdRamoAtividade() {
+		return cdRamoAtividade;
+	}
+
+	public void setCdRamoAtividade(String cdRamoAtividade) {
+		this.cdRamoAtividade = cdRamoAtividade;
+	}
+
+	public String getNmEmpresa() {
+		return nmEmpresa;
+	}
+
+	public String getDeEmail() {
+		return deEmail;
+	}
+
+	public String getDeSenha() {
+		return deSenha;
+	}
 
 }
